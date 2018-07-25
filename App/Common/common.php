be6aa4a9522d5102bcfa7cba9cf4727a54383533
jay chen <chenjiesuper@gmail.com>
2018-07-25 21:48:04 +0800

    format

--- App/Common/common.php
+++ App/Common/common.php
@@ -171,14 +171,13 @@ function ismobile() {
     return false;
  }
 
- function removeXSS($val)	{
+ function removeXSS($val) {
 	static $obj = null;
-        if ($obj === null) {
-            // 载入核心文件
-            require_once ("./Public/HTMLPurifier/HTMLPurifier.includes.php");
-            $obj = new HTMLPurifier();
-        }
- 
-        // 返回过滤后的数据
-        return $obj->purify($val);
+    if ($obj === null) {
+        // 载入核心文件
+        require_once ("./Public/HTMLPurifier/HTMLPurifier.includes.php");
+        $obj = new HTMLPurifier();
+    }
+    // 返回过滤后的数据
+    return $obj->purify($val);
 }
